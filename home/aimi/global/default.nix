4d4c2e29cba612d78086fba6dbbd02ef6773bacc
aimisaka <aimixsaka@gmail.com>
2023-01-30 12:22:28 +0800

    fix nix config

--- home/aimi/global/default.nix
+++ home/aimi/global/default.nix
@@ -41,11 +41,7 @@
     package = lib.mkDefault pkgs.nix;
     settings = {
       # Enable flakes and new 'nix' command
-      experimental-features = "nix-command flakes";
-      # Deduplicate and optimize nix store
-      auto-optimise-store = true;
-      substituters = [ "https://mirrors.ustc.edu.cn/nix-channels/store" "https://hyprland.cachix.org" ];
-      trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+      experimental-features = [ "nix-command flakes" "repl-flake" ];
     };
   };
 
