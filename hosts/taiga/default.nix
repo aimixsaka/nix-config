c95ac120ed2d3d65b56d570fc53b211987d53ba4
aimisaka <aimixsaka@gmail.com>
2023-02-02 10:32:32 +0800

    fix: fix surface hardware module name

--- hosts/taiga/default.nix
+++ hosts/taiga/default.nix
@@ -6,7 +6,7 @@
   # You can import other NixOS modules here
   imports = [
     # If you want to use modules from other flakes (such as nixos-hardware):
-    inputs.hardware.nixosModules.microsoft-surface-surface-pro-intel
+    inputs.hardware.nixosModules.microsoft-surface-pro-intel
 
     # You can also split up your configuration and import pieces of it here:
     # ./users.nix
