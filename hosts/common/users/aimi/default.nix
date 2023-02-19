257da0734c627cb89ac116a0493c5534545fa189
aimisaka <aimixsaka@gmail.com>
2023-02-19 10:41:09 +0800

    chore: add ssh service

--- hosts/common/users/aimi/default.nix
+++ hosts/common/users/aimi/default.nix
@@ -10,9 +10,10 @@ in {
     initialPassword = "aimi";
     isNormalUser = true;
     shell = pkgs.zsh;
-    #openssh.authorizedKeys.keys = [
+    openssh.authorizedKeys.keys = [
       # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
-    #];
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCtCi7o7AdRFMiV3qa3GBD7Qa2U/fFllRJfo4Mre9Z1XSyPQe5dAHd3CVwjI+8FcU48w2BIwKitU8JC32KGFm/VoRew0mg5YiBiTBKimISpzAvS+GUwGg86q0ZX/FyuQcuEHISOFueuBR9dZd2nB6TydlZDICuuypH/lXtUFarXicNhlwGozSX2kqWedeAnd3/iKfWIXu63u0BmHoAa050Zi2TqRwzE2OQ7AvKaDbaVqVXZc0KQ8azfM7IskOk6Vu34TwMK1CmCk1Rkp0OR9I7KZ/dx9tVAc73PTx0+fefUFMiNfpdihcmYRdlRkN/QLwgrVLu/hwa4ycI5mNCIVMFi3cYF5aY7xYbR9Igi52NFTEBN0wtInajocU6KGY86oQayI6sPNFhrPFZApV0bxWjHprZfOGw+OoJTqL0BElyaqzoSbFRvtNgWgJD33UVgBJrSIduLfwtTFGPOQY5Jg2Lux+UnSQzXBRLr4DBZ1Tyynv6jUsRWHPIA2nS3GXSX5GXlhBSQuEgTBDWfWwtBoR1ljWikDb/qykJbRBrMlVunLl1/XhXY57Kjojei193sIQ7VZJA4oZJMpKOBQBIA/6mDtffZpJGZMbUmq+fr4pQddZn2J7KZIOURoWObW1n2y7soWCMX2CoZljup/seM9NplE3HN1D0OCVFT1+k1A1MuEQ== aimi@taiga"
+    ];
     # TODO: Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
     extraGroups = [ 
       "wheel" 
--- hosts/taiga/default.nix
+++ hosts/taiga/default.nix
@@ -86,13 +86,16 @@
 
   # This setups a SSH server. Very important if you're setting up a headless system.
   # Feel free to remove if you don't need it.
-  #services.openssh = {
-  #  enable = true;
-  #  # Forbid root login through SSH.
-  #  permitRootLogin = "no";
-  #  # Use keys only. Remove if you want to SSH using password (not recommended)
-  #  passwordAuthentication = false;
-  #};
+  services.openssh = {
+    enable = true;
+    ports = [ 52842 ];
+    # Forbid root login through SSH.
+    settings = {
+      PermitRootLogin = "no";
+      # Use keys only. Remove if you want to SSH using password (not recommended)
+      PasswordAuthentication = false;
+    };
+  };
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
