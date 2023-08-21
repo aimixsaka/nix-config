6732cf2bf8984ad68571b58b0329e3562528f533
aimisaka <aimixsaka@gmail.com>
2023-08-21 14:46:36 +0800

    feat: add disko script

---
+++ scripts/install.sh
@@ -0,0 +1,1 @@
+nix --extra-experimental-features nix-command --extra-experimental-features flakes run github:nix-community/disko -- --mode zap_create_mount resources/system/disk_layout/luks-btrfs-tmproot.nix
