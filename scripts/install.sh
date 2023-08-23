13a227a214a34e8e77008b57ec91b870c5f6d02c
aimixsaka <aimixsaka@gmail.com>
2023-08-23 18:17:12 +0800

    scripts/install.sh: /dev/vda -> /dev/nvme0n1

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,4 +2,4 @@ read -p "Please input your password for luks: " -r psk
 echo -n "$psk" > /tmp/secret.key
 
 
-nix --extra-experimental-features nix-command --extra-experimental-features flakes run --option substituters "https://mirrors.ustc.edu.cn/nix-channels/store" github:nix-community/disko -- --mode disko "$1" --arg disks '[ "/dev/sda" ]'
+nix --extra-experimental-features nix-command --extra-experimental-features flakes run --option substituters "https://mirrors.ustc.edu.cn/nix-channels/store" github:nix-community/disko -- --mode disko "$1" --arg disks '[ "/dev/nvme0n1" ]'
