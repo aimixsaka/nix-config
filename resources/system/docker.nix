df1fd9008c84914e4ca3cfeef01f3947b12267d8
aimixsaka <aimixsaka@gmail.com>
2023-08-24 20:06:48 +0800

    docker: rootless -> rootful

--- resources/system/docker.nix
+++ resources/system/docker.nix
@@ -1,11 +1,11 @@
 {
   # Enable docker on btrfs
-  #virtualisation.docker.enable = true;
-  #virtualisation.docker.storageDriver = "btrfs";
+  virtualisation.docker.enable = true;
+  virtualisation.docker.storageDriver = "btrfs";
 
   # rootless
-  virtualisation.docker.rootless = {
-    enable = true;
-    setSocketVariable = true;
-  };
+  #virtualisation.docker.rootless = {
+  #  enable = true;
+  #  setSocketVariable = true;
+  #};
 }
