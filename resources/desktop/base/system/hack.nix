c3da83cd73088b45bbf160f2acf87c2bd5fa89d4
aimixsaka <aimixsaka@gmail.com>
2023-08-31 18:16:42 +0800

    fix: fix swaylock password wrong error

--- resources/desktop/base/system/hack.nix
+++ resources/desktop/base/system/hack.nix
@@ -3,4 +3,12 @@
   #error: GDBus.Error:org.freedesktop.DBus.Error.ServiceUnknown: The name ca.desrt.dconf was not provided by any .service files
   # when using gtk.enable = true
   programs.dconf.enable = true;
+
+
+  # fix swaylock always wrong password error
+  security.pam.services.swaylock = {
+    text = ''
+      auth include login
+    '';
+  };
 }
