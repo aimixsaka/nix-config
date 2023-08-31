2cd13bbb600a74661c8f64c96b14e58c4602809b
aimixsaka <aimixsaka@gmail.com>
2023-08-31 18:52:55 +0800

    fix(scripts): try to fix install.sh param parse error

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,4 +2,7 @@ read -p "Please input your password for luks: " -r psk
 echo -n "$psk" > /tmp/secret.key
 
 
-nix --extra-experimental-features nix-command --extra-experimental-features flakes run --option substituters "https://mirrors.ustc.edu.cn/nix-channels/store" github:nix-community/disko -- --mode disko "$1" --arg disks '[ "/dev/nvme0n1" ]'
+nix --extra-experimental-features nix-command \
+	--extra-experimental-features flakes \
+	run --option substituters "https://mirrors.ustc.edu.cn/nix-channels/store" \
+	github:nix-community/disko -- --mode disko "${2:-resources/system/disk_layout/luks-btrfs-tmproot.nix}" --arg disks "[ \"$1\" ]"
